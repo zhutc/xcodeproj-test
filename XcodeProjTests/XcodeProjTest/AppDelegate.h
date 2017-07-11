7ab65a14004b96f4d0bbd3d1e3b70af327f3e96d
tczhu <tczhu@Ctrip.com>
2017-07-11 19:29:47 +0800

    【中】wtf

---
+++ XcodeProjTests/XcodeProjTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XcodeProjTest
+//
+//  Created by 朱天超 on 2017/7/7.
+//  Copyright © 2017年 朱天超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XcodeProjTests/XcodeProjTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XcodeProjTest
+//
+//  Created by 朱天超 on 2017/7/7.
+//  Copyright © 2017年 朱天超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
